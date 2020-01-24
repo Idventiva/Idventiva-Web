90bcb9a6c8763d61a86f6cb79e3bc4396e8ac605
Héctor Reyes <h.reyes@idventiva.com>
2020-01-24 12:27:50 -0600

    Paginas de Servicios y Caso de Estudi agregadas

---
+++ service-page.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+<main class="service">
+
+    <section class="service__pilar">
+        
+        <!-- Line -->
+        
+        <section class="service__info">
+            <h4 class="service__subtitle">Creativos y Creadores</h4>
+            <h2 class="service__title">Branding y Diseño Digital</h2>
+            <p class="service__paragraph"> Texto de relleno</p>
+        </section>
+        <section class="service__cta">
+            <button class="service__button">¡A crear!</button>
+        </section>
+        
+        <div class="service__list">
+            <div class="service__item">
+                <i class="service__item__icon"></i>
+                <h5 class="service__item__name"></h5>
+                <ul class="service__item__list">
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+        
+        <section class="project-case">
+            <div class="project-case__item">
+                <div class="project-case__info">
+                    <h6></h6>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <button class="project-case__item__button"></button>
+                </div>
+            </div>
+        </section>
+        
+    </section>
+
+</main>
+
+<?php get_footer(); ?> 
