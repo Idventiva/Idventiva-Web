42b84f59ad04f627583e4491645ded88711ea1fb
Héctor Reyes <h.reyes@idventiva.com>
2020-01-31 12:54:58 -0600

    Ajustes de cajas en pagina de Servicios

--- servicios-page.php
+++ servicios-page.php
@@ -3,9 +3,11 @@
 <main class="service">
 
     <section class="section__title">
-        <h5>Nuestro</h5>
-        <h2>Trabajo</h2>
+        <h5>Nuestros</h5>
+        <h2>Servicios</h2>
     </section>
+
+    <!-------------------------------------- Branding y Diseño Digital -->
 
     <section class="service__pilar">
         
@@ -14,7 +16,7 @@
         <section class="service__info">
             <h5>Creativos y Creadores</h5>
             <h2>Branding y Diseño Digital</h2>
-            <p> Texto de relleno</p>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
         </section>
         <section class="service__cta">
           <button class="button--black">
@@ -51,7 +53,9 @@
 
         <h3>Así es como nos superamos</h3>
         
-        <section class="project-case">
+    </section>
+
+    <section class="project-case">
             <div class="project-case__item">
                 <div class="project-case__info">
                     <h3>Healthy Pau</h3>
@@ -66,10 +70,10 @@
             </div>
             <div class="project-case__item">
                 <div class="project-case__info">
-                    <h3>Toy Chavito</h3>
+                    <h3>Healthy Pau</h3>
                     <ul>
                         <li>Identidad Visual</li>
-                        <li>Manuales de identidad</li>
+                        <li>Marketing Digital</li>
                     </ul>
                 </div>
                 <button class="button--black">
@@ -77,9 +81,8 @@
                 </button>
             </div>
         </section>
-        
-    </section>
 
+<!-------------------------------------- Marketing Digital -->
 
     <section class="service__pilar">
         
@@ -88,7 +91,7 @@
         <section class="service__info">
             <h5>Planeamos y diseñamos el futuro</h5>
             <h2>Marketing Digital</h2>
-            <p> Texto de relleno</p>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
         </section>
         <section class="service__cta">
           <button class="button--black">
@@ -113,6 +116,7 @@
               <h4>Campañas de SEO y SEM</h4>
               <ul class="service__item__list">
                   <li>Google Analytics</li>
+              </ul>
             </div>
             
             <div class="service__item">
@@ -129,20 +133,188 @@
 
         <h3>Así es como nos superamos</h3>
         
-        <section class="project-case">
-            <div class="project-case__item">
-                <div class="project-case__info">
-                    <h3>Healthy Pau</h3>
-                    <ul>
-                        <li>Identidad Visual</li>
-                        <li>Marketing Digital</li>
-                    </ul>
-                </div>
-                <button class="button--black">
-                  <a href="/portafolio/healthypau">¡Descubrir!</a>
-                </button>
+    </section>
+
+    <section class="project-case">
+        <div class="project-case__item">
+            <div class="project-case__info">
+                <h3>Healthy Pau</h3>
+                <ul>
+                    <li>Identidad Visual</li>
+                    <li>Marketing Digital</li>
+                </ul>
             </div>
+            <button class="button--black">
+                <a href="/portafolio/healthypau">¡Descubrir!</a>
+            </button>
+        </div>
+        <div class="project-case__item">
+            <div class="project-case__info">
+                <h3>Healthy Pau</h3>
+                <ul>
+                    <li>Identidad Visual</li>
+                    <li>Marketing Digital</li>
+                </ul>
+            </div>
+            <button class="button--black">
+                <a href="/portafolio/healthypau">¡Descubrir!</a>
+            </button>
+        </div>
+        
+    </section>
+
+<!-------------------------------------- Producción Audiovisual -->
+
+    <section class="service__pilar">
+        
+        <!-- Line -->
+        
+        <section class="service__info">
+            <h5>Planeamos y diseñamos el futuro</h5>
+            <h2>Producción Audiovisual</h2>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
         </section>
+        <section class="service__cta">
+          <button class="button--black">
+            <a href="/contacto">¡A crear!</a>
+          </button>
+        </section>
+        
+        <div class="service__list">
+            <div class="service__item">
+                <i></i>
+                <h4>Fotografía Digital</h4>
+            </div>
+            
+            <div class="service__item">
+              <i></i>
+              <h4>Producción de Video</h4>
+              <ul class="service__item__list">
+                  <li>Videos Corporativos</li> 
+                  <li>Promocionales</li> 
+              </ul>
+            </div>
+            
+            <div class="service__item">
+              <i></i>
+              <h4>Post-Producción</h4>
+              <ul class="service__item__list">
+                  <li>Edición de Video</li> 
+                  <li>Motion Graphics</li> 
+              </ul>
+            </div>
+            
+            <div class="service__item">
+              <i></i>
+              <h4>Animación Digital</h4>
+            </div>
+        </div>
+
+        </div>
+
+        <h3>Así es como nos superamos</h3>
+        
+    </section>
+
+    <section class="project-case">
+        <div class="project-case__item">
+            <div class="project-case__info">
+                <h3>Healthy Pau</h3>
+                <ul>
+                    <li>Identidad Visual</li>
+                    <li>Marketing Digital</li>
+                </ul>
+            </div>
+            <button class="button--black">
+                <a href="/portafolio/healthypau">¡Descubrir!</a>
+            </button>
+        </div>
+        <div class="project-case__item">
+            <div class="project-case__info">
+                <h3>Healthy Pau</h3>
+                <ul>
+                    <li>Identidad Visual</li>
+                    <li>Marketing Digital</li>
+                </ul>
+            </div>
+            <button class="button--black">
+                <a href="/portafolio/healthypau">¡Descubrir!</a>
+            </button>
+        </div>
+        
+    </section>
+
+<!-------------------------------------- Diseño Web y Productos Digitales -->
+
+    <section class="service__pilar">
+        
+        <!-- Line -->
+        
+        <section class="service__info">
+            <h5>Planeamos y diseñamos el futuro</h5>
+            <h2>Diseño Web y Productos Digitales</h2>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+        </section>
+        <section class="service__cta">
+          <button class="button--black">
+            <a href="/contacto">¡A crear!</a>
+          </button>
+        </section>
+        
+        <div class="service__list">
+            <div class="service__item">
+                <i></i>
+                <h4>Sitios Web Corporativos</h4>
+              </ul>
+            </div>
+            
+            <div class="service__item">
+              <i></i>
+              <h4>Landing Pages</h4>
+            </div>
+            
+            <div class="service__item">
+              <i></i>
+              <h4>Blog e E-Commerce</h4>
+            </div>
+            
+            <div class="service__item">
+              <i></i>
+              <h4>Diseño de Aplicaciones</h4>
+            </div>
+        </div>
+
+        </div>
+
+        <h3>Así es como nos superamos</h3>
+        
+    </section>
+
+    <section class="project-case">
+        <div class="project-case__item">
+            <div class="project-case__info">
+                <h3>Healthy Pau</h3>
+                <ul>
+                    <li>Identidad Visual</li>
+                    <li>Marketing Digital</li>
+                </ul>
+            </div>
+            <button class="button--black">
+                <a href="/portafolio/healthypau">¡Descubrir!</a>
+            </button>
+        </div>
+        <div class="project-case__item">
+            <div class="project-case__info">
+                <h3>Healthy Pau</h3>
+                <ul>
+                    <li>Identidad Visual</li>
+                    <li>Marketing Digital</li>
+                </ul>
+            </div>
+            <button class="button--black">
+                <a href="/portafolio/healthypau">¡Descubrir!</a>
+            </button>
+        </div>
         
     </section>
 
