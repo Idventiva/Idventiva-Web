0522c2bbb944354a3da59d11f7b446369caf3417
Héctor Reyes <h.reyes@idventiva.com>
2020-02-20 14:57:00 -0600

    Automatización de Single conseguida

--- single.php
+++ single.php
@@ -1,62 +1,77 @@
 <?php get_header(); ?>
 
-	<main role="main" class="post-container">
-	<!-- section -->
-	<div class="sidebar-idv">
-		<?php get_sidebar(); ?>
-	</div>
-	<section class="main-area">
+<main class="single-project">
+	<?php if(have_posts()){ 
+			while (have_posts()){ 
+				the_post();
+			?> 
+				<section class="single-project__title">
+					<h5>Sitio Web</h5>
+					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				</section>
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+				<section class="single-project__intro">
+					<div class="single-project__info">
+						<h5>Descripción:</h5>
+						<p>Excepteur velit culpa culpa incididunt ad esse pariatur dolor eu irure do Lorem reprehenderit. Reprehenderit id ex occaecat excepteur culpa do velit reprehenderit et in eu incididunt veniam adipisicing. Tempor id qui dolore tempor eu id veniam.</p>
+						<h5>Categorías:</h5>
+						<ul>
+							<li>Identidad Visual</li>
+							<li>Naming</li>
+							<li>Registro de Marca</li>
+							<li>Publicidad</li>
+						</ul>
+						<button class="slide--left"><a href="#">Mi marca</a></button>
+					</div>
+					<div class="single-project__img">
+						<?php the_post_thumbnail('large'); ?>
+					</div>
+				</section>
 
-		<!-- article -->
-    <header class="post-header" style="background-image:url(<?php the_post_thumbnail_url() ?>);">
+				<section class="single-project__content">
+					<?php the_content(); ?>
+					<?php the_tags( __( 'Tags: ', 'idv2018' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+				</section>
+			<?php 
+			}
+		} ?>
 
+	<!-- Layout Manual de Entrada de Caso de Estudio -->
 
-			<!-- post title -->
-			<h1 class="section-title">
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
-
-			<!-- post details -->
-      <div class="post-meta">
-			<span class="date"><?php the_time('F j, Y'); ?> | <?php the_time('g:i a'); ?> |</span>
-			<span class="author"><?php _e( 'Publicado por', 'idv2018' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Deja tus comentarios', 'idv2018' ), __( '1 Comentario', 'idv2018' ), __( '% Comentarios', 'idv2018' )); ?></span>
-      </div>
-			<!-- /post details -->
-
-      </header>
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-			<?php the_content(); // Dynamic Content ?>
-
-			<?php the_tags( __( 'Tags: ', 'idv2018' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-
-
-		</article>
-		<!-- /article -->
-  <?php comments_template(); ?>
-
-	<?php endwhile; ?>
-
-	<?php else: ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php _e( 'Sorry, nothing to display.', 'idv2018' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+    <!-- <section class="single-project__content">
+        <div class="single-project__content__img--half">
+            <img src="img/slides/slide01.jpg" alt="">
+            <div class="single-project__content__info--left">
+                <h5>Año:</h5>
+                <p>2019</p>
+                <h5>Logros:</h5>
+                <ul>
+                    <li>Logro 01</li>
+                    <li>Logro 01</li>
+                    <li>Logro 01</li>
+                </ul>
+            </div>
+        </div>
+        <div class="single-project__content__img--complete">
+            <img src="img/slides/slide01.jpg" alt="">
+        </div>
+        <div class="single-project__content__img--half">
+            <div class="single-project__content__info">
+                <h2>Frase que destaque el proyecto o testimonio proporcionado por el cliente.</h2>
+            </div>
+            <img src="img/slides/slide02.jpg" alt="">
+        </div>
+        <div class="single-project__content__img--half">
+            <img src="img/slides/slide03.jpg" alt="">
+            <img src="img/slides/slide03.jpg" alt="">
+        </div>
+        <div class="single-project__content__img--complete">
+            <img src="img/slides/slide01.jpg" alt="">
+        </div>
+        <div class="center">
+            <button class="slide--left"><a href="#">Mi marca</a></button>
+        </div>
+    </section> -->
+</main>
 
 <?php get_footer(); ?>
